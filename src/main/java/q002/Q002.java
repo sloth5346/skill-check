871728fcbd09347247277d0a94c4922d7e31ebbe
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-21 18:44:33 +0900

    課題2

--- src/main/java/q002/Q002.java
+++ src/main/java/q002/Q002.java
@@ -45,5 +45,36 @@ public class Q002 {
             "14,林",
             "9,清水"
     };
+    
+    // main
+    public static void main(String[] args) {
+    	for(String str:sort(dataList)) {
+    		System.out.println(str);
+    	}
+    }
+    
+    // 昇順にソートする関数(バブルソート使用)
+    private static String[] sort(String[] defaultAry) {
+    	
+    	// 元の配列をコピー
+    	String[] array = new String[defaultAry.length];
+    	System.arraycopy(defaultAry, 0, array, 0, defaultAry.length);
+    	
+    	for(int i = 0; i < array.length - 1; i++) {
+    		for(int j = 0; j < array.length - i - 1; j++) {
+    			if(getId(array[j]) > getId(array[j + 1])) {
+    				String str = array[j + 1];
+    				array[j + 1] = array[j];
+    				array[j] =str;
+    			}
+    		}
+    	}
+    	return array;
+    }
+    
+    // IDを抜き出すメソッド
+    private static int getId(String str) {
+    	return Integer.parseInt(str.substring(0, str.indexOf(',')));
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 0時間 40分
